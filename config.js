2afeea56dde13bd868cd91362e620c0a3b79a036
pimg <pim.gaemers@gmail.com>
2024-10-23 10:52:49 +0200

    add non-normative guidelines to core for creating a Group
    Issue: fsc-nlx#478

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ let respecConfig = {
 
     previousMaturity: "VV",
     title: "FSC - Core",
-    content: {"draft-fsc-core-00-introduction": "", "draft-fsc-core-00-architecture": "", "draft-fsc-core-00-specifications": ""},
+    content: {"draft-fsc-core-00-introduction": "", "draft-fsc-core-00-guidelines":"informative", "draft-fsc-core-00-architecture": "", "draft-fsc-core-00-specifications": ""},
     editors:
         [
             {
